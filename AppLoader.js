5fc73b7a2b2e63ea92a98065470332abcaee1716
operman-code <jagtapomkar3052002@gmail.com>
2025-07-22 13:47:47 +0530

    Create AppLoader.js

---
+++ AppLoader.js
@@ -0,0 +1,43 @@
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  errorIcon: {
+    fontSize: 60,
+    marginBottom: 20,
+  },
+  errorTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    marginBottom: 12,
+    textAlign: 'center',
+  },
+  errorMessage: {
+    fontSize: 16,
+    color: '#fecaca',
+    textAlign: 'center',
+    marginBottom: 16,
+    lineHeight: 24,
+  },
+  errorRetry: {
+    fontSize: 14,
+    color: '#bfdbfe',
+    textAlign: 'center',
+    fontStyle: 'italic',
+  },
+  footer: {
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  footerText: {
+    fontSize: 12,
+    color: '#93c5fd',
+    marginBottom: 4,
+  },
+  versionText: {
+    fontSize: 10,
+    color: '#6b7280',
+  },
+});
+
+export default AppLoader;
