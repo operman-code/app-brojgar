173dca94af2fc9a19aa6876b39094c78ab74af46
operman-code <jagtapomkar3052002@gmail.com>
2025-07-22 19:17:16 +0530

    Create _layout.js

---
+++ app/_layout.js
@@ -0,0 +1,13 @@
+// app/_layout.js
+ import React from 'react';
+ import { Stack } from 'expo-router';
+ 
+ export default function RootLayout() {
+   return (
+     <Stack screenOptions={{ headerShown: false }}>
+       <Stack.Screen name="(tabs)" />
+       <Stack.Screen name="invoice" />
+       <Stack.Screen name="invoice-template" />
+     </Stack>
+   );
+ }
