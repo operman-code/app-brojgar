4a41d989c5fb7bab6045fe0edbda9716ec577653
operman-code <jagtapomkar3052002@gmail.com>
2025-07-23 01:08:02 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,6 @@
-// babel.config.js
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // Remove expo-router/babel line since it's deprecated in SDK 50
   };
 };
