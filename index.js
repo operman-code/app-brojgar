d6113bb18bd844f4302ccbe22cf768dc68bde4d7
operman-code <jagtapomkar3052002@gmail.com>
2025-07-22 20:30:31 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-// index.js - Main entry point with database initialization
-import { registerRootComponent } from 'expo';
-import AppLoader from './AppLoader';
-
-// Register the AppLoader as the main component
-registerRootComponent(AppLoader);
