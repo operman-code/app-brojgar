bb8100d59e124f1837b9facd972142e17bfc4fd1
operman-code <jagtapomkar3052002@gmail.com>
2025-07-22 11:46:54 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 import BottomTabNavigator from "./navigation/BottomTabNavigator";
-import InvoiceScreen from "./screens/Invoice/InvoiceScreen";
-import InvoiceTemplateScreen from "./screens/Invoice/InvoiceTemplateScreen";
-
-const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator 
-        screenOptions={{ headerShown: false }}
-        initialRouteName="Main"
-      >
-        <Stack.Screen name="Main" component={BottomTabNavigator} />
-        <Stack.Screen name="Invoice" component={InvoiceScreen} />
-        <Stack.Screen name="InvoiceTemplate" component={InvoiceTemplateScreen} />
-      </Stack.Navigator>
+      <BottomTabNavigator />
     </NavigationContainer>
   );
 }
