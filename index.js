0e5c2d2549a19b5176896488d9e06f2b4edeac32
operman-code <jagtapomkar3052002@gmail.com>
2025-07-22 13:50:09 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// index.js - Main entry point with database initialization
+import { registerRootComponent } from 'expo';
+import AppLoader from './AppLoader';
+
+// Register the AppLoader as the main component
+registerRootComponent(AppLoader);
