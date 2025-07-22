b30a493f6b42cad358cf9ac0aa841f32e43b5c16
operman-code <jagtapomkar3052002@gmail.com>
2025-07-22 19:34:08 +0530

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,28 +1,3 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
 const config = getDefaultConfig(__dirname);
-
-// Add support for web platform
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
-
-// Configure web-specific settings
-config.server = {
-  ...config.server,
-  enhanceMiddleware: (middleware, server) => {
-    return (req, res, next) => {
-      // Fix MIME type for JavaScript bundles
-      if (req.url && req.url.includes('.bundle')) {
-        res.setHeader('Content-Type', 'application/javascript; charset=utf-8');
-      }
-      return middleware(req, res, next);
-    };
-  },
-};
-
-// Enable web support
-config.transformer = {
-  ...config.transformer,
-  assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-};
-
-module.exports = config;
+ 
+ module.exports = config;
